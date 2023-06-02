1fda82a20aeb6a1c854e34b26a9c469dc9d4d952
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-06-02 18:51:10 +0100

    RequireSignIn middleware.

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,17 @@
 import express from "express"
 import { forgotPasswordController, loginController, registerController } from "../controllers/authController.js"
+import { requireSignIn } from "../middlewares/authMiddlewares.js"
 const router = express.Router()
 
 router.post("/register", registerController)
+
 router.post("/login", loginController)
+
 router.post("/forgot-password", forgotPasswordController)
 // router.post("/login", loginController)
+
+router.get("/user-auth", requireSignIn, (req, res) => {
+	res.status(200).send({ ok: true })
+})
 
 export default router
